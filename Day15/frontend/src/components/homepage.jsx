176f19ab8a776d99d21c52a8617b48e0ebe57038
Vibinprasath <126143671+Vibinprasath@users.noreply.github.com>
2023-07-28 15:24:46 +0530

    Add files via upload

---
+++ Day15/frontend/src/components/homepage.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import './homepage.css';
+
+
+function HomePage(){
+    return(
+<div className="wrap">
+  <div className="title text-center">
+     <h2>HOMEPAGE </h2>
+    
+  </div>
+  <div className="block-image image-1" />
+  <div className="block-image image-2" />
+  <div className="block-image image-3" />
+  <div className="block-image image-4" />
+</div>
+    );
+};
+
+
+export default HomePage;
