0a0d53023275b5cdb944b417bba8cda9d900838c
Vibinprasath <126143671+Vibinprasath@users.noreply.github.com>
2023-07-28 15:18:30 +0530

    Add files via upload

---
+++ Day11/backend/src/main/java/com/example/demo/Volunteer1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Volunteer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Volunteer1Application.class, args);
+	}
+
+}
